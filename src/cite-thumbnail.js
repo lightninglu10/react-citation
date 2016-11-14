2339e24ad4c1c54754162e24198a0fa6adc865c2
chadfennell <libsys@gmail.com>
2016-11-14 13:12:39 -0700

    ensures thumbnails occupy a row

--- src/cite-thumbnail.js
+++ src/cite-thumbnail.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Thumbnail = props => (<div className="col-md-2 row">
-                              <a className="thumbnail" hgref={props.url}>
-                                <img src={props.src} /><span className="glyphicon glyphicon-download-alt"></span> {props.text}
-                              </a>
+const Thumbnail = props => (<div className="row">
+                              <div className="col-md-2">
+                                <a className="thumbnail" hgref={props.url}>
+                                  <img src={props.src} /><span className="glyphicon glyphicon-download-alt"></span> {props.text}
+                                </a>
+                              </div>
                             </div>)
 
 const propTypes = {
@@ -15,3 +17,4 @@ const propTypes = {
 Thumbnail.propTypes = propTypes
 
 export default Thumbnail
+
