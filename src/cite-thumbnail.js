a23fe8331f1c27e9217e300f73c4998e3659cb39
chadfennell <libsys@gmail.com>
2016-11-15 12:42:39 -0700

    ain't no such thing as an hgref

--- src/cite-thumbnail.js
+++ src/cite-thumbnail.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CiteThumbnail = props => (<div className="row">
                               <div className="col-md-2">
-                                <a className="thumbnail" hgref={props.url}>
+                                <a className="thumbnail" href={props.url}>
                                   <img src={props.src} /><span className="glyphicon glyphicon-download-alt"></span> {props.text}
                                 </a>
                               </div>
