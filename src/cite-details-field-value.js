1a8c0dd1c3a5b81bef86206e633730b70f52f146
chadfennell <fenne035@umn.edu>
2017-05-08 15:44:50 -0500

    support the old link syntax along side html delimiters

--- src/cite-details-field-value.js
+++ src/cite-details-field-value.js
@@ -3,19 +3,43 @@ import React from 'react'
 export default class DetailsFieldValue extends React.Component {
   constructor(props) {
     super(props)
-    this._fields = this._fields.bind(this)
+    this._field =  this._field.bind(this)
+    this._link  =  this._link.bind(this)
+    this._danger = this._danger.bind(this)
+    this._delimiter = this._delimiter.bind(this)
+    this._text = this._text.bind(this)
   }
 
   _createMarkup(value) {
     return ({__html: value})
   }
 
-  _fields() {
-    return this._createMarkup(`${this.props.text}${this.props.delimiter}`)
+  _link() {
+    return `<a href="${this.props.url}">${this.props.text}</a>`
+  }
+
+  _field() {
+    if (this.props.url) {
+      return this._danger(this._link())
+    } else {
+      return this._danger(this.props.text)
+    }
+  }
+
+  _danger(html) {
+    return <div dangerouslySetInnerHTML={this._createMarkup(html)} />
+  }
+
+  _text() {
+    return (this.props.text == 'undefined') ? this._danger(this.props.text) : ''
+  }
+
+  _delimiter() {
+    return (this.props.delimiter == 'undefined') ? this._danger(this.props.delimiter) : ''
   }
 
   render() {
-    return <span dangerouslySetInnerHTML={this._fields()}></span>
+    return <span>{this._field()}{this._delimiter()}</span>
   }
 }
 
